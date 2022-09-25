1ddbbc4e9ddee0a81e11728e80d8563969fc3fbe
Guilherme Albuquerque <ngsalbuquerque@gmail.com>
2022-09-24 22:52:50 -0300

    a

---
+++ src/arvore/No.java
@@ -0,0 +1,14 @@
+package arvore;
+
+public class No {
+    
+    int valor;
+    No esquerda, direita;
+
+    public No (int valor) {
+        this.valor = valor;
+    }
+
+    public static No raiz;
+    //https://www.youtube.com/watch?v=Dg7ObtAwQvs
+}
